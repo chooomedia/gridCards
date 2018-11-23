9dce6e8fd59360fb021cff846a3d5071b494a6c4
Chris Matt <chooom@gmx.de>
2018-11-23 14:54:00 +0100

    changed gitignore first tries with an maria-db database-connection

--- js/widget.js
+++ js/widget.js
@@ -1,14 +1,15 @@
 class Widget {
     constructor(parentContainerId) {
         this.domElement = document.createElement("div");
-        
+
         if (typeof parentContainerId !== "string") {
             return;
         }
         let parentContainer = document.getElementById(parentContainerId);
         if (!parentContainer) {
-             return;      
+            return;
         }
         parentContainer.appendChild(this.domElement);
     }
+
 }
