581d816c394d1f54e1674f3d219a38a64f34e307
Chris Matt <chooom@gmx.de>
2018-12-08 07:38:20 +0100

    optimized dimensions images

--- js/_.js
+++ js/_.js
@@ -30,8 +30,7 @@ class AdCard extends Card {
     setCardTypeIcon(adHost) {
         let cardIconType = document.createElement("i");
         cardIconType.style.position = "absolute";
-        cardIconType.style.color = "#0099CC";
-        cardIconType.style.zIndex = "999";
+        cardIconType.style.color = "#888";
         cardIconType.style.bottom = "4px";
         cardIconType.style.right = "4px";
         cardIconType.style.width = "32px";
@@ -40,6 +39,7 @@ class AdCard extends Card {
         cardIconType.style.lineHeight = "32px";
         cardIconType.style.display = "inline-grid";
         cardIconType.style.textAlign = "center";
+        cardIconType.style.textShadow = "0px 0px 8px rgba(76, 76, 76, 0.7)";
 
         cardIconType.className = "fab fa-" + adHost;
         return cardIconType;
@@ -48,42 +48,34 @@ class AdCard extends Card {
     // Creates an affiliate linked element
     createAmznImgEl(adId) {
         let product = document.createElement("img");
-        product.style.minWidth = "100%";
-        product.style.height = "200px";
-        product.style.background = "#FFF";
-        product.style.padding = "13px 0";
-        product.style.border = "1px solid #0099CC";
-        product.style.position = "relative";
-        product.style.top = "0";
-        product.style.left = "0";
-        product.style.transform = "rotateY(180deg) scale(1)";
+        iframe.width = "101%";
+        iframe.height = "138px";
+        iframe.style.marginTop = "13px";
+        product.style.transform = "rotateY(180deg) scaleY(1.2)";
+        product.style.border = "0";
         product.src = "//ws-eu.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=DE&ASIN=" + adId + 
-                      "&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL640_&tag=" + this.partnerId;
+                      "&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=" + this.partnerId;
 
         product.link = "https://www.amazon.de/gp/product/" + adId +
                        "/ref=as_li_tl?ie=UTF8&camp=1638&creative=6742&creativeASIN=" + adId + 
-                       "&linkCode=as2&tag=" + this.partnerId;
+                       "&linkCode=as2&tag=" + this.partnerId + "&linkId=bb90198dddc380b6b2cd1a2e6765d4bd";
         product.onclick = () => {
             window.open(product.link, '_blank');
         };
         return product;
     }
 
-    // Creates a object element
+    // Creates a Iframe element
     createAmznIframe(adId) {
-        let iframe = document.createElement("iframe");
-        iframe.style.height = "241px";
-        iframe.style.width = "100%";
-        iframe.style.transform = "rotateY(180deg) scaleX(.94) !important";
-        iframe.style.border = "0px";
-        iframe.style.right = "36%";
-        iframe.style.top = "-7px";
-        iframe.style.position = "relative";
-        iframe.style.display =  "inline-grid";
-        iframe.src = "//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=DE&source=ac&ref=tf_til&ad_type=product_link&tracking_id="+ this.partnerId + "&marketplace=amazon&region=DE&placement="+ adId +"&asins="+ adId +"&show_border=false&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff";
-        iframe.onclick = () => {
+        let iFrame = document.createElement("iframe");
+        iFrame.style.width = "auto";
+        iFrame.style.height = "164px";
+        iFrame.style.transform = "rotateY(180deg) scaleY(1.2)";
+        iFrame.style.border = "0";
+        iFrame.src = "//ws-eu.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=DE&source=ac&ref=tf_til&ad_type=product_link&tracking_id="+ this.partnerId + "&marketplace=amazon&region=DE&placement="+ adId +"&asins="+ adId +"&linkId=076baa6f59ed86500d03962249018761&show_border=false&link_opens_in_new_window=true&price_color=333333&title_color=0066c0&bg_color=ffffff";
+        iFrame.onclick = () => {
             window.open(product.link, '_blank');
         };
-        return iframe;
+        return iFrame;
     }
 }
